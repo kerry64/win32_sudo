b628c4b833f6015f25f2a6853e0773578f55a435
kerry64 <79123545+kerry64@users.noreply.github.com>
2021-03-17 19:42:32 +0800

    Update sudo.bat
    reduce recursive calls

--- sudo.bat
+++ sudo.bat
@@ -33,47 +33,37 @@ set tmp_prefix=sudo_neverconflict
 
 set args_$UD0=%*
 set args_$UD0=!args_$UD0:^"=^\^"!
-if "!control_$UD0!"=="yes" set args_$UD0=
-if "%~1"=="!magic_12byte!" goto:dosth
+set "cleaner_$UD0=cd /d "!tmp:~!"^&del /f /s /q !tmp_prefix!_*.tmp.reg ^>nul 2^>nul"
+if "%~1"=="!magic_12byte!" (set "args_$UD0=!args_$UD0:~13!"&goto:dosth)
 if "%~1"=="_hide"      (set unhide_$UD0=0& goto:control_admin)
 if "%~1"=="_unhide"    (set unhide_$UD0=1& goto:control_admin)
-if "%~1"=="_transform" (set trans_$UD0=add& goto:transform)
-if "%~1"=="_restore"   (set trans_$UD0=delete& goto:transform)
-if "%~1"=="_clean"     (goto:cleanup)
+if "%~1"=="_transform" (set args_$UD0=net localgroup Administrators !username! /add)
+if "%~1"=="_restore"   (set args_$UD0=net localgroup Administrators !username! /delete
+                        set pwwd_$UD0=!cd:~!&goto:dosth)
+if "%~1"=="_clean"     (%cleaner_$UD0%&goto:end)
 set pwwd_$UD0=!cd!
-cd /d !windir!
-runas /noprofile /env /user:!admin_name! "%~dpnx0 !magic_12byte! !args_$UD0!"
+:action
+cd /d "!windir!"
+runas /noprofile /env /user:!admin_name! "cmd.exe /d /c %~dpnx0 !magic_12byte! !args_$UD0!"
 goto:end
 
 :dosth
+if not "!regi_$UD0!"=="" (set "args_$UD0=!regi_$UD0!"&set pwwd_$UD0=!tmp:~!)
 set pwwd_$UD0=!pwwd_$UD0:^"=!
-if "!control_$UD0!"=="yes" cd /d "!tmp!" & set args_$UD0=!magic_12byte! reg.exe import !hide_$UD1!.reg
-start /min powershell -Command "Start-Process cmd -Verb RunAs -WindowStyle Minimized -ArgumentList '/d /c cd /d !pwwd_$UD0!&&start !args_$UD0:~13!'"
+start /min powershell -Command "Start-Process cmd.exe -Verb RunAs -WindowStyle Minimized -ArgumentList '/d /c cd /d !pwwd_$UD0!&&start !args_$UD0!'"
 endlocal
 goto:end
 
 :control_admin
-set control_$UD0=yes
 set hide_$UD1=!tmp_prefix!_%random%.tmp
-cd /d "!tmp!"
-del /f /s /q !tmp_prefix!_*.tmp.reg >nul 2>nul
-rem cleanup previous data
-if "!control_$UD0!"=="yes" (
-echo Windows Registry Editor Version 5.00
-echo;
+%cleaner_$UD0%
+(
+echo Windows Registry Editor Version 5.00&echo.
 echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList]
 echo "!admin_name!"=dword:0000000!unhide_$UD0!
 )>!hide_$UD1!.reg
-"%~dpnx0"
-goto:end
-
-:transform
-"%~dpnx0" net localgroup Administrators !username! /!trans_$UD0!
-rem cleanup by the way
-
-:cleanup
-cd /d "!tmp!"
-del /f /s /q !tmp_prefix!_*.tmp.reg >nul 2>nul
+set regi_$UD0=reg.exe import !hide_$UD1!.reg
+goto:action
 
 :end
 endlocal
